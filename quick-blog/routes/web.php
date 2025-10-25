9af87d233bc9e0c87298802fc0154ddfbc860700
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-10-25 20:28:59 +0600

    updated dashboard

--- quick-blog/routes/web.php
+++ quick-blog/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
@@ -29,4 +31,25 @@ Route::middleware(['auth'])->group(function(){
     
     Route::post('/posts/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
 
+    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    Route::patch('/profile/picture', [UserController::class, 'profilePicture'])->name('profile.picture');
+    Route::get('/profile/update', [UserController::class, 'editBio'])->name('profile.editBio');
+    Route::patch('/profile/update', [UserController::class, 'bioUpdate'])->name('profile.bioUpdate');
+
+});
+
+
+Route::middleware(['auth', 'admin'])->group(function(){
+    Route::get('/admin/dashboard/categories', [CategoryController::class, 'show'])->name('categories.show');
+    
+    Route::get('/admin/dashboard/categories/create', [CategoryController::class, 'create'])->name('categories.create.get');
+    Route::post('/admin/dashboard/categories/create', [CategoryController::class, 'store'])->name('categories.create.post');
+    
+    Route::get('/admin/dashboard/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit.get');
+    Route::patch('/admin/dashboard/categories/{id}/edit', [CategoryController::class, 'update'])->name('categories.edit.post');
+   
+    Route::delete('/admin/dashboard/categories/{id}/delete', [CategoryController::class, 'delete'])->name('categories.delete');
+
+    
+    Route::get('/admin/users', [UserController::class, 'users'])->name('admin.users');
 });
